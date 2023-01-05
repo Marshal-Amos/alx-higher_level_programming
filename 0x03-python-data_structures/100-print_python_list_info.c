ad3b7b98aef14b72a84d5577a4cc98d8087cb414
Marshal Amos <amosmarshal@gmail.com>
2023-01-05 15:11:01 -0800

    100-print_python_list_info.c

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "Python.h"
+
+void print_python_list_info(PyObject *p)
+{
+	PyListObject *pp;
+	int i;
+
+	pp = p;
+	printf("[*] Size of the Python List = %d\n", pp->ob_base.ob_size);
+	printf("[*] Allocated = %d\n", pp->allocated);
+
+	for (i = 0; i < pp->ob_base.ob_size; i++);
+	{
+		printf("Element %d: %s\n", i, pp->ob_item[i]->ob_type->tp_name);
+	}
+}
